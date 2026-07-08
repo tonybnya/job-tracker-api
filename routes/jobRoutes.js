607262143d7dbe6cc4c5cf92555b74a544c69283
tonybnya <tonybnya@gmail.com>
2026-07-08 14:00:57 +0100

    feat: define routes and apply authentication middleware to all endpoints

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -7,5 +7,18 @@
 
 const express = require('express');
 const router = express.Router();
+const jobController = require('../controllers/jobController');
+const validateBody =  require('../middleware/validateMiddleware');
+const { jobSchema } = require('../validators/appSchemas');
+const { authenticateToken } = require('../middleware/authMiddleware');
+
+// apply authentication middleware to all endpoints in this router file
+router.use(authenticateToken);
+
+router.post('/', validateBody(jobSchema), jobController.createJob);
+router.get('/', jobController.getJobs);
+router.get('/:id', jobController.getJobById);
+router.put('/:id', validateBody(jobSchema), jobController.updateJob);
+router.delete('/:id', jobController.deleteJob);
 
 module.exports = router;
