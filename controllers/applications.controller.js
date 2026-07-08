23ab186044e31df8e870038183827aff16cdc400
tonybnya <tonybnya@gmail.com>
2026-07-08 10:02:43 +0100

    chore: define file structure

---
+++ controllers/applications.controller.js
@@ -0,0 +1,7 @@
+/**
+ * Script Name : applications.controller.js
+ * Description : Describe what this JavaScript/TypeScript file does
+ * Usage       : node applications.controller.js
+ * Author      : @tonybnya
+ */
+
---
+++ routes/applications.routes.js
@@ -0,0 +1,7 @@
+/**
+ * Script Name : applications.routes.js
+ * Description : Describe what this JavaScript/TypeScript file does
+ * Usage       : node applications.routes.js
+ * Author      : @tonybnya
+ */
+
