a6b3f139abaa6ee1376caa520698e71712e0855d
tonybnya <tonybnya@gmail.com>
2026-07-08 14:05:30 +0100

    feat: define auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,11 @@
 
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
+const validateBody = require('../middleware/validateMiddleware');
+const { registerSchema, loginSchema } = require('../validators/appSchemas');
+
+router.post('/register', validateBody(registerSchema), authController.register);
+router.post('/login', validateBody(loginSchema), authController.login);
 
 module.exports = router;
