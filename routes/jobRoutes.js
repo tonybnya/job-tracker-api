eb7e895a4ae37187ccfc842de7ee512eb0edf0ed
tonybnya <tonybnya@gmail.com>
2026-07-08 11:18:29 +0100

    feat: define skeleton for Job routes

---
+++ routes/jobRoutes.js
@@ -0,0 +1,11 @@
+/**
+ * Script Name : jobRoutes.js
+ * Description : Routes layer for job application resources
+ * Usage       : node jobRoutes.js
+ * Author      : @tonybnya
+ */
+
+const express = require('express');
+const router = express.router();
+
+module.exports = router;
